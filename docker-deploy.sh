3a5d37f7f9bdcd16987efded9f8171b8b37a04ee
Sebastian Javier Marchano <sebasjm@gmail.com>
2016-12-22 11:59:17 -0300

    20161222_1159: added docker deploy

---
+++ docker-deploy.sh
@@ -0,0 +1,9 @@
+[ -z "$1" ] && echo "missing change's description" && exit 1
+set -x
+npm run build || exit 1
+version=$(date +"%Y%m%d_%H%M")
+message="$version: $@"
+git ci -am "$message" || (echo 'could not commit, had you made changes?' && exit 1)
+docker tag my-user-space eu.gcr.io/navigator-1320/my-user-space:$version
+gcloud docker -- push eu.gcr.io/navigator-1320/my-user-space:$version
+kubectl set image deployment my-user-space my-user-space=eu.gcr.io/navigator-1320/my-user-space:$version
