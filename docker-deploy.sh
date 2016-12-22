b674f43e5fb9ba777fe6003ef21cf8d364ce09e7
Sebastian Javier Marchano <sebasjm@gmail.com>
2016-12-22 12:01:58 -0300

    20161222_1201: missing docker build

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,6 +1,7 @@
 [ -z "$1" ] && echo "missing change's description" && exit 1
 set -x
 npm run build || exit 1
+bash docker-build.sh || exit 1
 version=$(date +"%Y%m%d_%H%M")
 message="$version: $@"
 git ci -am "$message" || (echo 'could not commit, had you made changes?' && exit 1)
